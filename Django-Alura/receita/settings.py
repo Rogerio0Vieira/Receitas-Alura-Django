8751fbf1bc838a819ccb568b5ff7d1453e5a1d85
Rogério Vieira <43701289+Rogerio0Vieira@users.noreply.github.com>
2021-02-12 15:19:30 -0300

    App "Completo"
    integração com banco de dados PostgreSQL, alteração do models, utilização do django Admin, criação de super usuário e exibindo os dados do banco em tela.

--- Django-Alura/receita/settings.py
+++ Django-Alura/receita/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'receita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_receita',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost'
     }
 }
 
