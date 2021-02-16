0ba6504d216609324c539e82e72ab0ec6a324daa
Rogério Vieira <43701289+Rogerio0Vieira@users.noreply.github.com>
2021-02-16 20:14:14 -0300

    Filtros e busca
    Adicionadas algumas funções no admin.py

--- Django-Alura/addreceita/admin.py
+++ Django-Alura/addreceita/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Receita
 
-admin.site.register(Receita)
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo')
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita', )
+    list_filter = ('categoria', )
+    list_per_page = 2
+    
+
+admin.site.register(Receita, ListandoReceitas)
